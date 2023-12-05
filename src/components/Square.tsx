f233631bba176e6ebca7bd278956195421ced97d
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2023-12-05 14:16:01 +0530

    winnig line of square styled

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,21 +3,6 @@ import { styled } from '@mui/system';
 import { SquareValue } from '../type/Type';
 import { Button } from '@mui/material';
 
-// const MyButtonStyled = styled('button')({
-//     width: '100px',
-//     height: '100px',
-//     padding: 0,
-//     cursor: 'pointer',
-//     backgroundColor: '',
-//     fontSize: '70px',
-//     color: 'white',
-//     '&:hover': {
-//         backgroundColor: '#3f51b5',
-//     },
-//     '&:actiive': {
-//         backgroundColor: 'black',
-//     },
-// });
 
 const MyButtonStyled = styled('button')<{
     $bgColor?: string;
@@ -28,11 +13,12 @@ const MyButtonStyled = styled('button')<{
     cursor: pointer;
     background-color: ${props => props.$bgColor};
     font-size: 70px;
-    color: white;
+    color: ${props => props.$bgColor === '#81c784' ? '#2e7d32' : '#81c784'};
+    text
     &:hover {
-        background-color: #3f51b5;
+        background-color: #1b5e20;
     }
-    &:active { /* Fixed the typo in :active */
+    &:active {
         background-color: black;
     }
 `;
